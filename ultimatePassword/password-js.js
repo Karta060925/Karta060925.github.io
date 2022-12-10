fe6eea7d67448ec2b78f5f6dc09ab0db6c9c7f8e
Sean <63107825+Karta060925@users.noreply.github.com>
2022-12-11 01:04:35 +0800

    password-js-update

--- ultimatePassword/password-js.js
+++ ultimatePassword/password-js.js
@@ -5,9 +5,17 @@ const btnClick = document.querySelector('.btn-enter')
 const btnValue = document.querySelector('.value')
 const numText = document.querySelector('p')
 
-let light = document.querySelector('.light')
+const light = document.querySelector('.light')
 
-const imgBomb = document.querySelector('.img-bomb')
+const imgSafe = document.querySelector('.img-safe')
+const imgBomb_0 = document.querySelector('.img-bomb-0')
+const imgBomb_1 = document.querySelector('.img-bomb-1')
+const imgBomb_2 = document.querySelector('.img-bomb-2')
+const imgBomb_3 = document.querySelector('.img-bomb-3')
+const imgBomb_4 = document.querySelector('.img-bomb-4')
+const imgBomb_5 = document.querySelector('.img-bomb-5')
+const imgBomb_6 = document.querySelector('.img-bomb-6')
+
 const imgBomber = document.querySelector('.img-bomber')
 const targetBlank = document.querySelector('.target-blank')
 let sectionBomb = document.querySelector('.section-bomb')
@@ -16,6 +24,7 @@ let num = numInput.value.text
 
 let max = 100
 let min = 0
+let errorCount = 0;
 
 // 產生隨機卵數
 function getRandomIntInclusive(min, max) {
@@ -60,8 +69,10 @@ btnResult.addEventListener('click', function () {
 let btnNumber = document.querySelectorAll('.btn-number')
 btnNumber.forEach(element => {
     element.addEventListener('click', function () {
-        numInput.value += element.innerHTML
+        numInput.value += element.innerHTML;
+
     })
+
 });
 
 // 監聽按鈕
@@ -70,6 +81,43 @@ btnClick.addEventListener('click', function () {
     let numInputValue = numInput.value
     let isParseInt = parseInt(numInput.value, 10)
 
+    // 限定猜的次數
+    errorCount++
+
+    if (errorCount > 6) {
+        imgBomb_6.style.display = 'none';
+        imgBomber.style.display = 'block';
+        sectionBomb.setAttribute('class', 'section-bomb bomber')
+    }
+    else if (errorCount == 6) {
+        imgBomb_5.style.display = 'none';
+        imgBomb_6.style.display = 'block';
+        light.style.background = 'radial-gradient(circle farthest-corner at 10% 20%, rgba(247, 87, 0, 1) 0%, rgba(249, 0, 0, 1) 90.1%)';
+    }
+    else if (errorCount == 5) {
+        imgBomb_4.style.display = 'none';
+        imgBomb_5.style.display = 'block';
+    }
+    else if (errorCount == 4) {
+        imgBomb_3.style.display = 'none';
+        imgBomb_4.style.display = 'block';
+        light.style.background = 'linear-gradient( 112.7deg,  rgba(253,185,83,1) 11.4%, rgba(255,138,0,1) 70.2% )';
+    }
+    else if (errorCount == 3) {
+        imgBomb_2.style.display = 'none';
+        imgBomb_3.style.display = 'block';
+
+    }
+    else if (errorCount == 2) {
+        imgBomb_1.style.display = 'none';
+        imgBomb_2.style.display = 'block';
+
+    }
+    else if (errorCount == 1) {
+        imgBomb_0.style.display = 'none';
+        imgBomb_1.style.display = 'block';
+
+    }
 
     // 判斷輸入是否是數字
     if (isNaN(isParseInt)) {
@@ -80,18 +128,29 @@ btnClick.addEventListener('click', function () {
         // 判斷輸入的值範圍
         if (isParseInt == numValue) {
             warnText.innerText = ''
-            light.setAttribute('class', 'light danger')
-            imgBomb.style.display = 'none';
-            imgBomber.style.display = 'block';
+            // light.setAttribute('class', 'light danger')
+            imgBomb_0.style.display = 'none';
+            imgBomb_1.style.display = 'none';
+            imgBomb_2.style.display = 'none';
+            imgBomb_3.style.display = 'none';
+            imgBomb_4.style.display = 'none';
+            imgBomb_5.style.display = 'none';
+            imgBomb_6.style.display = 'none';
+            imgSafe.style.display = 'block';
+
+            // light.style.background = 'radial-gradient(circle farthest-corner at 10% 20%, rgba(247, 87, 0, 1) 0%, rgba(249, 0, 0, 1) 90.1%)';
+            // imgBomber.style.display = 'block';
             targetBlank.style.display = 'inline-block';
-            sectionBomb.setAttribute('class', 'section-bomb bomber')
+            // sectionBomb.setAttribute('class', 'section-bomb bomber')
             numText.innerText = `恭喜你猜對了，答案是 ${numValue}`
+            numText.style.color = '#00dc7f';
         }
         else if (isParseInt < numValue && isParseInt > min) {
             min = isParseInt
             numText.innerText = `${min} ~ ${max}`
             numInput.value = ''
             warnText.innerText = ''
+
 
         }
         else if (isParseInt > numValue && isParseInt < max) {
@@ -108,5 +167,11 @@ btnClick.addEventListener('click', function () {
 
         }
     }
+
+
+
+
+
+
 })
 
