d2b00d79335c81dcd4910dbfaf57cc1e3f0098ec
Sean <63107825+Karta060925@users.noreply.github.com>
2022-12-18 00:33:13 +0800

    menu

---
+++ menu/menu.js
@@ -0,0 +1,14 @@
+let hamburger = document.querySelector('.hamburger');
+let btnArrow = document.querySelector('.btn-arrow');
+let dropdownItem = document.querySelector('.dropdown-item');
+
+let dropdownSecond = document.querySelector('.dropdown-item');
+
+btnArrow.addEventListener('click', function () {
+    dropdownItem.style.display = 'block';
+})
+// hamburger.addEventListener('click', function () {
+//     dropdownSecond.style.display = 'block';
+
+// })
+
