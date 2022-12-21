be3f3dc369d2146ea67406c21174a611c3b3ab01
Sean <63107825+Karta060925@users.noreply.github.com>
2022-12-22 02:24:34 +0800

    LOL

---
+++ LOL/lol.js
@@ -0,0 +1,82 @@
+// 英雄資料陣列
+let heroArray = [];
+let container;
+let resilt;
+let btnCreateHeroArray, btnListHero, btnTableHero;
+let tbody;
+
+window.onload = function () {
+    // 選到 #container
+    container = document.querySelector("#container");
+    // 抓到 btn 建立&清除
+    btnCreateHeroArray = document.getElementById("createhero");
+    // 抓到btn 顯示文字
+    btnListHero = document.getElementById("listhero");
+    // 抓到btn 顯示表格
+    btnTableHero = document.getElementById("tablehero");
+    // 抓到 #result(顯示表格)
+    result = document.getElementById("result");
+    tbody = document.querySelector('tbody');
+
+    // btn 建立&清除 監聽
+    btnCreateHeroArray.addEventListener("click", function () {
+        alert("從DOM建立英雄人物陣列資料成功！");
+
+        // 抓到 #container 裡的 .card
+        let card = container.querySelectorAll('.card');
+        card.forEach((data, index) => {
+            // .card 裡的 .card-img-top(圖片)
+            let img = data.querySelector('.card-img-top');
+            // .card 裡的 .card-text(敘述)
+            let text = data.querySelector('.card-text');
+            heroArray.push({
+                index: index,
+                name: data.id,
+                text: text.innerText,
+                img: img.getAttribute('src')
+            });
+        });
+
+        console.log(heroArray);
+
+        // 清除 container
+        document.querySelector("#container").innerHTML = "";
+    });
+
+
+    // btn 顯示文字 監聽
+    btnListHero.addEventListener("click", function () {
+        if (heroArray.length == 0) {
+            alert("請先建立陣列資料");
+            return;
+        }
+
+        // 清空
+        tbody.innerHTML = "";
+        result.style.display = 'none';
+
+        // 顯示 heroArray[]內容 到 HTML
+        heroArray.forEach(data => {
+            container.innerHTML += `<div class="col-4 mb-3"><div class="item"><p>編號：${data.index}</p><p>名稱：${data.name}</p><p>敘述：${data.text}</p><p>圖片：<br>${data.img}</p></div></div>`;
+        });
+    });
+
+
+
+    // btn 顯示表格
+    btnTableHero.addEventListener("click", function () {
+        if (heroArray.length == 0) {
+            alert("請先建立陣列資料");
+            return;
+        }
+        // 清空
+        result.style.display = 'block';
+        document.querySelector("#container").innerHTML = "";
+
+        // 顯示 heroArray[]內容 到 HTML
+        heroArray.forEach(data => {
+            tbody.innerHTML += `<tr><th scope="row">${data.index}</th><td>${data.name}</td><td>${data.text}</td><td>${data.img}</td></tr>`;
+
+        });
+    });
+}
